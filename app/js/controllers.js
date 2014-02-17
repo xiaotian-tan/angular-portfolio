182c22d52b5ed6980f8d3f97b62f05ac50bf921a
Xiaotian Tan <xiaotiantan@192-168-1-103.tpgi.com.au>
2014-02-17 21:07:32 +1100

    add project slider

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -5,6 +5,30 @@
 angular.module('myApp.controllers', []).
   controller('ProjectController', ['$scope', 'Portfolio', function($scope, Portfolio) {
 
+  	// not show project detail as default
+  	$scope._Index = -1;
+
+  	// get projects from service
   	$scope.projects = Portfolio.query();
+
+  	// detect if current project slide is active
+  	$scope.isActive = function (index){
+        return $scope._Index === index;
+    };
+
+    // show previous project
+    $scope.showPrev = function (index){
+		$scope._Index = ($scope._Index > 0) ? --$scope._Index : $scope.projects.length - 1;
+    };
+
+    // show next project
+    $scope.showNext = function (index){
+    	$scope._Index = ($scope._Index < $scope.projects.length - 1) ? ++$scope._Index : 0;
+    };
+
+    // show project on click
+    $scope.showProject = function (index){
+    	$scope._Index = index;
+    };
 
   }]);
