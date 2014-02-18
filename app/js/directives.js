c109cc98230fa4fe170f898070f627d7f31c24fa
Xiaotian Tan <xiaotiantan@192-168-1-103.tpgi.com.au>
2014-02-19 01:04:42 +1100

    install angular bootstrap

--- app/js/directives.js
+++ app/js/directives.js
@@ -4,32 +4,33 @@
 
 
 angular.module('myApp.directives', []).
-  directive('appVersion', ['version', function(version) {
-    return function(scope, elm, attrs) {
-      elm.text(version);
-    };
-  }]).
-  directive('activeLink', function($location) {
- 
-    var link = function(scope, element, attrs) {
-        scope.$watch(function() { return $location.path(); },
-        function(path) {
-            var url = element.find('a').attr('href');
-            if (url) {
-                url = url.substring(1);
-            }
- 
-            if (path == url) {
-                element.addClass("active");
-            } else {
-                element.removeClass('active');
-            }
- 
-        });
-    };
- 
-    return {
-        restrict: 'A',
-        link: link
-    };
-});
+    directive('appVersion', ['version', function(version) {
+        return function(scope, elm, attrs) {
+          elm.text(version);
+        };
+    }]).
+    directive('activeLink', function($location) {
+        var link = function(scope, element, attrs) {
+            scope.$watch(function() { 
+                return $location.path(); 
+            },
+            function(path) {
+                var url = element.find('a').attr('href');
+                if (url) {
+                    url = url.substring(1);
+                }
+     
+                if (path == url) {
+                    element.addClass("active");
+                } else {
+                    element.removeClass('active');
+                }
+     
+            });
+        };
+     
+        return {
+            restrict: 'A',
+            link: link
+        };
+    });
