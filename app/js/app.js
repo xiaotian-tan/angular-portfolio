072870564677bbb07bf310e0ecf707ddcbd95595
Xiaotian Tan <xiaotiantan@192-168-1-106.tpgi.com.au>
2014-02-25 18:10:10 +1100

    ng-include footer and add animation of ng-view

--- app/js/app.js
+++ app/js/app.js
@@ -14,10 +14,34 @@ angular.module('myApp', [
   'myApp.services',
 ]).
 config(['$routeProvider', function($routeProvider) {
-  $routeProvider.when('/projects', {templateUrl: 'partials/projects.html', controller: 'ProjectsController'});
-  $routeProvider.when('/projects/:projectId', {templateUrl: 'partials/project.html', controller: 'ProjectController'});
-  $routeProvider.when('/', {templateUrl: 'partials/welcome.html', controller: 'WelcomeController'});
-  $routeProvider.when('/contact', {templateUrl: 'partials/contact.html', controller: 'ContactController'});
+  $routeProvider.when('/projects', {
+    templateUrl: 'partials/projects.html', 
+    controller: 'ProjectsController',
+    pageKey: 'projects'
+  });
+  $routeProvider.when('/projects/:projectId', {
+    templateUrl: 'partials/project.html', 
+    controller: 'ProjectController',
+    pageKey: 'projects'
+  });
+  $routeProvider.when('/', {
+    templateUrl: 'partials/welcome.html', 
+    controller: 'WelcomeController',
+    pageKey: 'home'
+  });
+  $routeProvider.when('/contact', {
+    templateUrl: 'partials/contact.html', 
+    controller: 'ContactController',
+    pageKey: 'contact'
+  });
 
   $routeProvider.otherwise({redirectTo: '/'});
+}]).
+run(['$rootScope', '$http', '$browser', '$timeout', "$route", "$location", "$window", 
+  function ($scope, $http, $browser, $timeout, $route, $location, $window) {
+  $scope.$on("$routeChangeSuccess", function (scope, next, current) {
+    $scope.page = $route.current.pageKey;
+    console.log($location.path());
+    $window._gaq.push(['_trackPageview', $location.path()]);
+  });
 }]);
