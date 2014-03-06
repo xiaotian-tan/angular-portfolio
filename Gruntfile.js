9a4c20868239828e9a02b730a92faa75736b7936
Xiaotian Tan <xiaotiantan@192-168-1-106.tpgi.com.au>
2014-03-07 01:10:01 +1100

    use grunt to watch compass sass and update styling

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,20 +1,46 @@
 module.exports = function(grunt) {
 
 
-	grunt.initConfig({
-	  imagemin: {   
-	    dynamic: {                         // Another target
-	      files: [{
-	        expand: true,                  // Enable dynamic expansion
-	        cwd: 'app/img/projects/',                   // Src matches are relative to this path
-	        src: ['**/*.{png,jpg,gif}'],   // Actual patterns to match
-	        dest: 'app/img/projects-thumb/'                  // Destination path prefix
-	      }]
-	    }
-	  }
-	});
+    grunt.initConfig({
+    	pkg: grunt.file.readJSON('package.json'),
+        compass: { // Task
+            dist: { // Target
+                options: { // Target options
+                	basePath: 'app',
+                    sassDir: 'sass',
+                    cssDir: 'css',
+                    environment: 'production'
+                }
+            },
+            dev: { // Another target
+                options: {
+                	basePath: 'app',
+                    sassDir: 'sass',
+                    cssDir: 'css'
+                }
+            }
+        },
+        watch: {
+			css: {
+				files: '**/*.scss',
+				tasks: ['compass:dist']
+			}
+		},
+        imagemin: {
+            dynamic: { // Another target
+                files: [{
+                    expand: true, // Enable dynamic expansion
+                    cwd: 'app/img/projects/', // Src matches are relative to this path
+                    src: ['**/*.{png,jpg,gif}'], // Actual patterns to match
+                    dest: 'app/img/projects-thumb/' // Destination path prefix
+                }]
+            }
+        }
+    });
 
-	grunt.loadNpmTasks('grunt-contrib-imagemin');
-	grunt.registerTask('default', ['imagemin']);
+    grunt.loadNpmTasks('grunt-contrib-imagemin');
+    grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.loadNpmTasks('grunt-contrib-compass');
+    grunt.registerTask('default',['watch']);
 
 };
